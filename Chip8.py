bda4aaddbc3f5ab7a3a793abc2382a37625d983d
FestyWalrus <spsknybeln@gmail.com>
2020-07-08 12:32:47 -0400

    Emulation functional

--- Chip8.py
+++ Chip8.py
@@ -5,12 +5,77 @@
 '''
 
 import cpu
-#import pygame
+import os
+import sys
+import time
+import numpy as np
+import pygame
+from tkinter import Tk
+from tkinter.filedialog import askopenfilename
+
+Tk().withdraw() #Don't need a full GUI so close it
+
+CPS = 500 #cycles per second
+pygame.init()
+clock = pygame.time.Clock()
+cycle = 0
+
+scale = 3
+window = pygame.display.set_mode((64 * scale, 32 * scale))
+pygame.display.set_caption("Chip-8")
+
+
+black = (0, 0, 0)
+white = (255, 255, 255)
 
 chip = cpu.chip8()
 
-chip.load_rom("test")
+chose_rom = False
 
-print("Done loading\nExecuting one cycle")
 
-chip.emu_cycle()
+print("Done loading\n")
+
+while True:
+	clock.tick(CPS)
+
+	#get input
+	for event in pygame.event.get():
+		if event.type == pygame.QUIT:
+			pygame.quit()
+			sys.exit(0)
+
+		if event.type == pygame.KEYDOWN:
+			if event.key in chip.kbKeys:
+				chip.press_key(chip.kbKeys.index(event.key))
+
+		if event.type == pygame.KEYUP:
+			if event.key in chip.kbKeys:
+				chip.release_key(chip.kbKeys.index(event.key))
+
+		if event.type == pygame.MOUSEBUTTONUP:
+			if not chose_rom:
+				filename = askopenfilename()
+				chip.load_rom(filename)
+				chose_rom = True
+
+
+	#think
+
+	if chose_rom:
+		#print("fps: " + str(clock.get_fps()))
+		#print(str(cycle), end=" ")
+		cycle += 1
+		chip.emu_cycle()
+
+		#render
+		if chip.screen_update:
+			window.fill(black)
+			screen = chip.get_screen()
+			for xPos, x in enumerate(screen):
+				for yPos, y in enumerate(x):
+					if y == True:
+						pygame.draw.rect(window, white, (yPos * scale, xPos * scale, scale, scale))
+			pygame.display.update()
+			chip.screen_update = False
+
+	
--- cpu.py
+++ cpu.py
@@ -11,9 +11,22 @@
 '''
 
 import numpy as np
+import random
+import pygame
+import sys
 
 class chip8:
 	def __init__(self):
+		#0-F in numerical order, not how they appear on the keypad
+		self.kbKeys = [pygame.K_x, pygame.K_1, pygame.K_2, pygame.K_3,
+					pygame.K_q, pygame.K_w, pygame.K_e, pygame.K_a,
+					pygame.K_s, pygame.K_d, pygame.K_z, pygame.K_c,
+					pygame.K_4, pygame.K_r, pygame.K_f, pygame.K_v]
+
+		self.tone = pygame.mixer.Sound("tone.wav")
+
+		self.screen_update = False
+
 		self.opcode = bytearray(2)
 		self.opcode[0] = 0x00	#initialized to 0
 		self.opcode[1] = 0x00
@@ -31,17 +44,29 @@ class chip8:
 		self.index = 0
 		self.pc = 0x200 #program counter
 
-		self.stack = bytearray(16)
-		for i in range(0, len(self.stack)):
+		self.stack = np.zeros(16, dtype='int')
+		for i in range(0, 16):
 			self.stack[i] = 0
+
+		self.keys = np.zeros(16, dtype='bool')
+		for i in range(0, 16):
+			self.keys[i] = False
 
 		self.sp = 0 #stack pointer
 
 		#load fontset
 
 		#start with a black display
-		self.display = np.array((64, 32), dtype=bool)
-		self.display.fill(False)
+		self.display = np.zeros((32, 64), dtype='bool')
+
+		with open("font", "rb") as font:
+			byte = font.read(1)
+			memIndex = 0x0
+
+			while byte:
+				self.memory[memIndex] = byte[0]
+				memIndex += 1
+				byte = font.read(1)
 
 	def load_rom(self, filename):
 		#fill up the memory
@@ -56,26 +81,309 @@ class chip8:
 				byte = rom.read(1)
 
 
-
-
 	def emu_cycle(self):
 
 		#Step 1: Fetch!
 		self.opcode = self.memory[self.pc] << 8 | self.memory[self.pc + 1];
-		print("Opcode: " + str(hex(self.opcode)))
+		#print("Opcode: " + str(hex(self.opcode)) + " PC: " + str(hex(self.pc)))
 
 		#Step 2: Decode and Execute!
-		cmd = self.opcode & 0xF000
 
-		if cmd == 0xA000:	#ANNN: Sets I to the address NNN
-			self.index = self.opcode & 0x0FFF #set index to NNN
-			print("Updated index to " + str(hex(self.index)));
-			self.pc += 2 #advance program counter by 2
+		decoding = True
+		while decoding:
+			#F000 Opcode mask
+			cmd = self.opcode & 0xF000
+
+			if cmd == 0x1000:	#1NNN: Jumps to address NNN
+				self.pc = self.opcode & 0x0FFF #set program counter to NNN
+				break
+
+			if cmd == 0x2000:	#2NNN: calls subroutine at NNN
+				self.stack[self.sp] = self.pc + 2 #store current address + 2 in stack
+				self.sp += 1 #increment stack counter
+				if self.sp > 15:
+					print("E: The stack got smashed!")
+					self.sp = 15
+				self.pc = self.opcode & 0x0FFF #set program counter to NNN
+				break
+
+			if cmd == 0x3000:	#3XNN: skips next instruction if VX == NN
+				if self.registers[(self.opcode & 0x0F00) // 0x100] == self.opcode & 0x00FF:
+					self.pc += 4
+				else:
+					self.pc += 2
+				break
+
+			if cmd == 0x4000:	#0x4XNN: skips next instruction if VX != NN
+				if self.registers[(self.opcode & 0x0F00) // 0x100] != self.opcode & 0x00FF:
+					self.pc += 4
+				else:
+					self.pc += 2
+				break
+
+			if cmd == 0x6000:	#0x6XNN: Sets VX to NN
+				self.registers[(self.opcode & 0x0F00) // 0x100] = self.opcode & 0x00FF
+				self.pc += 2
+				break
+
+			if cmd == 0x7000:	#0x7XNN: Adds NN to VX (don't change carry flag)
+				if (self.registers[(self.opcode & 0x0F00) // 0x100] + self.opcode & 0x00FF) > 255:
+					self.registers[(self.opcode & 0x0F00) // 0x100] = (self.registers[(self.opcode & 0x0F00) // 0x100] + self.opcode & 0x00FF) - 256
+				else:
+					self.registers[(self.opcode & 0x0F00) // 0x100] = self.registers[(self.opcode & 0x0F00) // 0x100] + self.opcode & 0x00FF
+				self.pc += 2
+				break
+
+			if cmd == 0xA000:	#ANNN: Sets index to the address NNN
+				self.index = self.opcode & 0x0FFF #set index to NNN
+				self.pc += 2 #advance program counter by 2
+				break
+
+			if cmd == 0xB000:	#BNNN: Jumps to NNN + V0
+				self.pc = (self.opcode & 0x0FFF) + self.registers[0]
+				break
+
+			if cmd == 0xC000:	#CXNN: Set VX to random number AND NN
+				self.registers[(self.opcode & 0x0F00) // 0x100] = (random.randint(0, 255) & (self.opcode & 0x00FF))
+				self.pc += 2
+				break
+
+			if cmd == 0xD000:	#DXYN: Draw sprite on screen, read from index
+				self.screen_update = True
+				xPos = int(self.registers[(self.opcode & 0x0F00) // 0x100])
+				yPos = int(self.registers[(self.opcode & 0x00F0) // 0x10])
+				height = self.opcode & 0x000F
+				self.registers[0xF] = 0
+
+				byteOffset = 0
+
+				for i in range(0, height):
+					#print(str(hex(self.index + byteOffset)) + " " + hex(self.memory[self.index + byteOffset]))
+					bitRow = self.bits(self.memory[self.index + byteOffset])
+					if yPos + i > 31: #wrap around
+						yPos -= 32 #* yPos // 31
+					
+					for count, bit in enumerate(bitRow):
+						if xPos + count > 63: #wrap around
+							xPos -= 64
+
+						if bit:
+							if self.display[yPos + i][xPos + count] == True:
+								self.registers[0xF] = 1
+								
+							self.display[yPos + i][xPos + count] = not self.display[yPos + i][xPos + count]
+					byteOffset += 1
+
+				self.pc += 2
+				break
+
+			#F00F Opcode mask
+			cmd = self.opcode & 0xF00F
+
+			if cmd == 0x5000:	#5XY0: Skips next instruction if VX == VY
+				if self.registers[(self.opcode & 0x0F00) // 0x100] == self.registers[(self.opcode & 0x00F0) // 0x10]:
+					self.pc += 4
+				else:
+					self.pc += 2
+				break
+
+			if cmd == 0x8000:	#8XY0: Sets VX to the value of VY
+				self.registers[(self.opcode & 0x0F00) // 0x100] = self.registers[(self.opcode & 0x00F0) // 0x10]
+				self.pc += 2
+				break
+
+			if cmd == 0x8001:	#8XY1: Sets VX to VX OR VY
+				self.registers[(self.opcode & 0x0F00) // 0x100] = self.registers[(self.opcode & 0x0F00) // 0x100] | self.registers[(self.opcode & 0x00F0) // 0x10]
+				self.pc += 2
+				break
+
+			if cmd == 0x8002:	#8XY2: Sets VX to VX AND VY
+				self.registers[(self.opcode & 0x0F00) // 0x100] = self.registers[(self.opcode & 0x0F00) // 0x100] & self.registers[(self.opcode & 0x00F0) // 0x10]
+				self.pc += 2
+				break
+
+			if cmd == 0x8003:	#8XY3: Sets VX to VX XOR VY
+				self.registers[(self.opcode & 0x0F00) // 0x100] = self.registers[(self.opcode & 0x0F00) // 0x100] ^ self.registers[(self.opcode & 0x00F0) // 0x10]
+				self.pc += 2
+				break
+
+			if cmd == 0x8004:	#8XY4: Adds VY to VX, sets VF (carry) if necessary
+				if (self.registers[(self.opcode & 0x0F00) // 0x100] + self.registers[(self.opcode & 0x00F0) // 0x10]) > 255:
+					self.registers[(self.opcode & 0x0F00) // 0x100] = (self.registers[(self.opcode & 0x0F00) // 0x100] + self.registers[(self.opcode & 0x00F0) // 0x10]) - 256
+					self.registers[15] = 1
+				else:
+					self.registers[(self.opcode & 0x0F00) // 0x100] = self.registers[(self.opcode & 0x0F00) // 0x100] + self.registers[(self.opcode & 0x00F0) // 0x10]
+				self.pc += 2
+				break
+
+			if cmd == 0x8005:	#8XY5: Subtracts VY from VX, sets VF if there's no borrow
+				# https://github.com/JamesGriffin/CHIP-8-Emulator/blob/d1870538c009d3d76bd5d91319560edc58d16eae/src/chip8.cpp
+				if self.registers[(self.opcode & 0x00F0) >> 4] > self.registers[(self.opcode & 0x0F00) >> 8]:
+					self.registers[15] = 0 #there is a borrow
+					self.registers[(self.opcode & 0x0F00) >> 8] = (self.registers[(self.opcode & 0x0F00) >> 8] - self.registers[(self.opcode & 0x00F0) >> 4]) + 256
+				else:
+					self.registers[15] = 1
+					self.registers[(self.opcode & 0x0F00) >> 8] -= self.registers[(self.opcode & 0x00F0) >> 4]
+				self.pc += 2
+				break
+
+			if cmd == 0x8006:	#8XY6: Stores LSB of VX in VF and shifts VX right by 1
+				self.registers[15] = self.registers[(self.opcode & 0x0F00) // 0x100] & 0b1
+				self.registers[(self.opcode & 0x0F00) // 0x100] = self.registers[(self.opcode & 0x0F00) // 0x100] >> 1
+				self.pc += 2
+				break
+
+			if cmd == 0x8007:	#8XY7: Sets VX to VY-VX, VF 0 if borrow, 1 if no borrow
+				# https://github.com/JamesGriffin/CHIP-8-Emulator/blob/d1870538c009d3d76bd5d91319560edc58d16eae/src/chip8.cpp
+				if self.registers[(opcode & 0x0F00) >> 8] > self.registers[(opcode & 0x00F0) >> 4]:	# VY-VX
+					self.registers[15] = 0 # there is a borrow
+				else:
+					self.registers[15] = 1
+				self.registers[(opcode & 0x0F00) >> 8] = self.registers[(opcode & 0x00F0) >> 4] - self.registers[(opcode & 0x0F00) >> 8]
+				self.pc += 2
+				break
+
+			if cmd == 0x800E:	#8XYE: Stores MSB of VX in VF and shifts VX left by 1
+				self.registers[15] = (self.registers[(self.opcode & 0x0F00) // 0x100] & 0b1000000) >> 6
+				self.registers[(self.opcode & 0x0F00) // 0x100] = (self.registers[(self.opcode & 0x0F00) // 0x100] << 1) & 255
+				self.pc += 2
+				break
+
+			if cmd == 0x9000:	#9XY0: Skips next instruction if VX != VY
+				if self.registers[(self.opcode & 0x0F00) // 0x100] != self.registers[(self.opcode & 0x00F0) // 0x10]:
+					self.pc += 4
+				else:
+					self.pc += 2
+				break
+
+			#F0FF Opcode mask
+			cmd = self.opcode & 0xF0FF
+
+			if cmd == 0xE09E:	#EX9E: Skips next instruction if key in VX is pressed
+				if self.keys[self.registers[(self.opcode & 0x0F00) // 0x100]]:
+					self.pc += 4
+				else:
+					self.pc += 2
+				break
+
+			if cmd == 0xE0A1:	#EXA1: Skips next instruction if key in VX is not pressed
+				if not self.keys[self.registers[(self.opcode & 0x0F00) // 0x100]]:
+					self.pc += 4
+				else:
+					self.pc += 2
+				break
+
+			if cmd == 0xF007:	#FX07: Sets VX to value of delay timer
+				self.registers[(self.opcode & 0x0F00) // 0x100] = self.delay_timer
+				self.pc += 2
+				break
+
+			if cmd == 0xF00A:	#FX0A: Wait for keypress and store key in VX
+				while True:
+					event = pygame.event.wait()
+					if event.type == pygame.QUIT:
+						pygame.quit()
+						sys.exit(0)
+					if event.type == pygame.KEYDOWN:
+						print("Key was booped")
+						if event.key in self.kbKeys:
+							self.registers[(self.opcode & 0x0F00) // 0x100] = self.kbKeys.index(event.key)
+							break
+
+				self.pc += 2
+				break
+
+			if cmd == 0xF015:	#FX15: Sets delay timer to VX
+				self.delay_timer = self.registers[(self.opcode & 0x0F00) // 0x100]
+				self.pc += 2
+				break
+
+			if cmd == 0xF018:	#FX18: Sets sound timer to VX
+				self.sound_timer = self.registers[(self.opcode & 0x0F00) // 0x100]
+				self.pc += 2
+				break
+
+			if cmd == 0xF01E:	#FX1E: Adds VX to I without changing VF
+				self.index += self.registers[(self.opcode & 0x0F00) // 0x100]
+				self.pc += 2
+				break
+
+			if cmd == 0xF029:	#FX29: Sets I to the location of sprite for character in VX
+				self.index = self.registers[(self.opcode & 0x0F00) // 0x100] * 5	#each character is 5 bytes tall
+				self.pc += 2
+				break
+
+			if cmd == 0xF033:	#FX33: Stores binary-coded decimal of VX across 3 memory locations
+				num = self.registers[(self.opcode & 0x0F00) // 0x100]
+				num = str(num)
+
+				while(len(num) < 3): #make sure that the number is 3 digits
+					num = '0' + num
+
+				self.memory[self.index] = int(num[0])
+				self.memory[self.index + 1] = int(num[1])
+				self.memory[self.index + 2] = int(num[2])
+
+				self.pc += 2
+				break
+
+			if cmd == 0xF055:	#FX55: Stores V0 to VX inclusive in memory starting at I, don't change I
+				for offset in range(0, ((self.opcode & 0x0F00) // 0x100) + 1):
+					self.memory[self.index + offset] = self.registers[offset]
+				self.pc += 2
+				break
+
+			if cmd == 0xF065:	#FX65: Loads V0 to VX inclusive from memory starting at I, don't change I
+				for offset in range(0, ((self.opcode & 0x0F00) // 0x100) + 1):
+					self.registers[offset] = self.memory[self.index + offset]
+				self.pc += 2
+				break
+
+			#FFFF Opcode mask
+			cmd = self.opcode & 0xFFFF
+
+			if cmd == 0x00E0:	#00E0: Clears the display
+				self.display = np.zeros((32, 64), dtype='bool')
+				self.pc += 2
+				break
+
+			if cmd == 0x00EE:	#00EE: Returns from a subroutine
+				self.sp -= 1
+				self.pc = self.stack[self.sp]
+				break
+
+			print("Opcode not found: " + hex(self.opcode))
 
 		#Step 3: Update Timers!
 		if self.delay_timer > 0:
 			self.delay_timer -= 1
 
 		if self.sound_timer > 0:
-			printf("Beep!");
+			pygame.mixer.Sound.play(self.tone)
 			self.sound_timer -= 1
+
+	def get_screen(self):
+		return self.display
+
+	def bits(self, n):
+		bitString = bin(n)[2:]
+		bList = list()
+
+		if len(bList) != 8:
+			diff = 8 - len(bitString)
+			for i in range(0, diff):
+				bList.insert(0, False)
+
+		for pos, c in enumerate(bitString):
+			if c == '1':
+				bList.append(True)
+			else:
+				bList.append(False)
+
+		return bList
+
+	def press_key(self, index):
+		self.keys[index] = True
+
+	def release_key(self, index):
+		self.keys[index] = False
